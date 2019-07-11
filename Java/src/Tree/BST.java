cb6df7e32b22ef19c110c8c92dd9fa4c4c6ed9f5
SibilJoeKurian <sibil.kurian@gmail.com>
2019-07-11 02:43:33 -0400

    added main function

--- Java/src/Tree/BST.java
+++ Java/src/Tree/BST.java
@@ -1,45 +1,63 @@
 package Tree;
 
-public class BST {
-	class Node {
-		int data;
-		Node leftChild;
-		Node rightChild;
 
-		Node(int item) {
-			data = item;
+public class test1 {
+	class Node {
+		Node right;
+		Node left;
+		int data;
+
+		Node(int value) {
+			right = left = null;
+			data = value;
 		}
+
 	}
 
 	Node root;
 
-	BST() {
+	test1() {
 		root = null;
 	}
 
-	public void insert(int item) {
+	public void insert(int value) {
 		if (root == null)
-			root = new Node(item);
+			root = new Node(value);
 		else
-			insertNode(root, item);
+			insertNode(root, value);
 	}
 
-	private void insertNode(Node root, int item) {
-		// TODO Auto-generated method stub
-		Node tempNode = new Node(item);
-		if (root.data < item)
-			if (root.leftChild == null)
-				root.leftChild = tempNode;
-			else
-				insertNode(root.leftChild, item);
-		else if (root.data > item)
-			if (root.rightChild == null)
-				root.rightChild = tempNode;
-			else
-				insertNode(root.rightChild, item);
+	public void insertNode(Node root1, int value) {
+		Node tempNode=new Node(value);
+		if(root1.data>value)
+			if(root1.left==null)
+				root1.left=tempNode;
+			else 
+				insertNode(root1.left,value);
+		else if(root1.data<value)
+			if(root1.right==null)
+				root1.right=tempNode;
+			else 
+				insertNode(root1.right,value);
 	}
-	
+	public void inOrder() {
+		inOrderTraversal(root);
+	}
+	public void inOrderTraversal(Node root1) {
+		if (root1 == null) 
+            return;
+		inOrderTraversal(root1.left);
+		System.out.println(root1.data);
+		inOrderTraversal(root1.right);
+	}
 	public static void main(String[] args) {
-
+		test1 t1 = new test1();
+		t1.insert(4);
+		t1.insert(3);
+		t1.insert(5);
+		t1.insert(6);
+		t1.insert(2);
+		t1.insert(1);
+		t1.inOrder();
 	}
 }
